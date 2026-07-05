b1f24d53f987f2c1d031fc26950fcb75f71b03c5
HarithSheha <harithsheha1@gmail.com>
2026-07-06 01:14:29 +0300

    nimekosea kucommit commit iliotakiwa hapa nimeekkweny backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // src/lib/api.ts
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://172.18.8.21:8000/api';
 
 async function request(path: string, options: RequestInit = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
